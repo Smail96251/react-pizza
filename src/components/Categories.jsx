f5b786aaae6f1925013061ac6e137a4e9a5c86a6
Smail96251(Nikita) <hveshchenko@gmail.com>
2023-05-13 11:49:56 +0300

    feat: add render categories by using map() and on click active category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
+
 export const Categories = () => {
+	const [indexCategory, setIndexCategory] = useState(0);
+
+	const activeCategory = index => {
+		setIndexCategory(index);
+	};
+
+	const categories = [
+		'Все',
+		'Мясные',
+		'Вегетарианская',
+		'Гриль',
+		'Острые',
+		'Закрытые',
+	];
 	return (
 		<div className='categories'>
 			<ul>
-				<li className='active'>Все</li>
-				<li>Мясные</li>
-				<li>Вегетарианская</li>
-				<li>Гриль</li>
-				<li>Острые</li>
-				<li>Закрытые</li>
+				{categories.map((value, index) => (
+					<li
+						onClick={() => activeCategory(index)}
+						className={indexCategory === index ? 'active' : ''}
+					>
+						{value}
+					</li>
+				))}
 			</ul>
 		</div>
 	);
